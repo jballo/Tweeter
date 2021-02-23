effe697237fd55c4a7ccac638086cf5db93c4b25
Jonathan Ballona <jballona5127@gmail.com>
2021-02-22 16:28:16 -0800

    Added: Retweeting and fixed never ending refreshing logo

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -58,6 +58,8 @@ class HomeTableViewController: UITableViewController {
             
             self.tableView.reloadData()
             
+            self.refresher.endRefreshing() //end refreshing icon
+            
         }, failure: { (Error) in
             print("Could not retreive tweets!")
         })
@@ -88,7 +90,7 @@ class HomeTableViewController: UITableViewController {
         
         cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
-        
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -22,7 +22,7 @@ class TweetCellTableViewCell: UITableViewCell {
     
     var favorited:Bool = false
     var tweetId:Int = -1
-    
+//    var retweeted:Bool = false
     
     @IBAction func favoriteTweet(_ sender: Any) {
         
@@ -46,6 +46,22 @@ class TweetCellTableViewCell: UITableViewCell {
     
     @IBAction func retweet(_ sender: Any) {
         
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error is retweeting: \(error)")
+        })
+        
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
     }
     
     
